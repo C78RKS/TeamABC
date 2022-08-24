a95844f49be102cfeb344c15c856c8c6f2c3cdb2
Adia & Indy <107782632+ObviousEcho@users.noreply.github.com>
2022-08-23 19:46:53 -0600

    created basic html and file structure, created initial psuedo code in script.js

---
+++ assets/js/script.js
@@ -0,0 +1,53 @@
+// globally scoped var's
+// =====================================================================
+
+
+// write a function which makes a fetch/http request to the OMDB API
+// =====================================================================
+
+
+// write a function which dynamically creates elements to display
+// response data from OMDB request
+// =====================================================================
+
+
+// write a function which removes previously searched movie from document
+// so that incoming search data replaces previously searced data and does
+// not append below it
+// =====================================================================
+
+
+// write a function which makes a fetch/http request to the NYT API
+// =====================================================================
+
+
+// write a function which dynamically creates elements to display
+// response data from NYT request
+// =====================================================================
+
+
+// write a function which removes previous review from document
+// so that incoming review data replaces previous review data and does
+// not append below it
+// =====================================================================
+
+
+// write a function which gets local storage if it exists
+// =====================================================================
+
+
+// write a function which stores movies to local storage
+// =====================================================================
+
+
+// write a function which dynamically displays watchlist
+// =====================================================================
+
+
+// create an event listener for search bar submit form from document
+// =====================================================================
+
+
+// using event delegation, create an event listener for a button which 
+// saves movies to a watchlist
+// =====================================================================
